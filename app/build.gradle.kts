66f922ab1eaac5d719ea499b0910cea50a6bce26
ptitmouton <ptitmouton@users.noreply.github.com>
2024-10-31 15:58:59 +0000

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         applicationId = "net.einsa.lotta"
         minSdk = 28
         targetSdk = 34
-        versionCode = 14
+        versionCode = 15
         versionName = "0.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
